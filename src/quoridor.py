23f41b6f4ee1eb98870fcf31802d1af32fe5a217
Brennan Beal <brennan.beal@flatiron.com>
2024-05-19 02:14:28 -0400

    Remove print

--- src/quoridor.py
+++ src/quoridor.py
@@ -71,7 +71,6 @@ class Quoridor:
                             pygame.quit()
                             quit()
                         success = current_player.update(event, self.board, self.players)
-                        print(self.board.get_state())
                     self._render(current_player)
 
     def _render(self, current_player: Player):
