fb97058fe676ff08ec464b66f64f5b6ce7c3e6f3
Calder Hoover <calderhoover@gmail.com>
2024-05-19 01:31:42 -0400

    Extend metadata render to support arbitrary player count. (#3)

--- quoridor.py
+++ quoridor.py
@@ -1,13 +1,14 @@
 from src.assemble_board import assemble_board_component_groups
-from src.utils import display_player_walls
 from src.player import Player
 from pygame.sprite import Group
 from src.constants import *
 import pygame
 
+DEFAULT_FONT_SIZE = 32
+
 
 class Quoridor:
-    def __init__(self, players=None):
+    def __init__(self, players=None, font_size=DEFAULT_FONT_SIZE):
         pygame.init()
         pygame.display.set_caption("Quoridor")
         self.screen = pygame.display.set_mode((SCREEN_SIZE_X, SCREEN_SIZE_Y))
@@ -18,21 +19,22 @@ class Quoridor:
             self.players = players
         self.player_group = Group()
         self.player_group.add(self.players)
+        self.font_size = font_size
+        self.font = pygame.font.SysFont(None, font_size)
 
     @staticmethod
     def default_players():
         return [
-            Player(player_number=1, color=pygame.Color("coral"), position=(GAME_SIZE*0.5, HALF_DISTANCE), radius=0.5*CELL),
-            Player(player_number=2, color=pygame.Color("blue"), position=(GAME_SIZE*0.5, GAME_SIZE - HALF_DISTANCE), radius=0.5*CELL)
+            Player(name="Player 1", color=pygame.Color("coral"), position=(GAME_SIZE*0.5, HALF_DISTANCE), radius=0.5*CELL),
+            Player(name="Player 2", color=pygame.Color("blue"), position=(GAME_SIZE*0.5, GAME_SIZE - HALF_DISTANCE), radius=0.5*CELL),
+            Player(name="Player 3", color=pygame.Color("blue"), position=(GAME_SIZE*0.5, GAME_SIZE - HALF_DISTANCE), radius=0.5*CELL)
         ]
 
     def play_game(self):
-        current_player = 1
+        current_player_index = 0
         while True:
-            current_player_index = current_player - 1
-
-            player = self.players[current_player_index]
-            if player.is_ai:
+            current_player = self.players[current_player_index]
+            if current_player.is_ai:
                 raise RuntimeError("AI unimplemented")
                 # TODO: implement
                 # while True:
@@ -47,15 +49,33 @@ class Quoridor:
                         if event.type == pygame.QUIT:
                             pygame.quit()
                             quit()
-                        success = player.update(event, self.nodes, self.walls, self.players)
+                        success = current_player.update(event, self.nodes, self.walls, self.players)
+                    self._render(current_player)                     
+                   
+            current_player_index = (current_player_index + 1) % len(self.players)
 
-                    self.screen.fill(WHITE)
-                    self.walls.update()
-                    self.walls.draw(self.screen)
-                    self.nodes.draw(self.screen)
-                    self.player_group.draw(self.screen)
-                    display_player_walls(player_group=self.player_group, screen=self.screen, cur_play=current_player)
-                    pygame.display.flip()
+    def _render(self, current_player: Player):
+        self.screen.fill(WHITE)
+        self.walls.update()
+        self.walls.draw(self.screen)
+        self.nodes.draw(self.screen)
+        self.player_group.draw(self.screen)
+        self._render_metadata(current_player)
+        pygame.display.flip()
+    
+    def _render_metadata(self, current_player: Player):
+        self.screen.blit(
+            self.font.render(f"Current player: {current_player.name}", False, (0, 0, 0)), 
+            (GAME_SIZE, 0)
+        )
+        for i, player in enumerate(self.players):
+            row = self.font_size * (i + 1) * 2
+            self.screen.blit(
+                self.font.render(f"{player.name}", False, (0, 0, 0)),
+                (GAME_SIZE, row)
+            )
+            self.screen.blit(
+                self.font.render(f"Total walls: {player.total_walls}", False, (0, 0, 0)), 
+                (GAME_SIZE + 20, row + self.font_size)
+            )
 
-
-            current_player = (current_player % len(self.players)) + 1
