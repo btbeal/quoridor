687ea90a5c89a341d0b7ab7894f903f0943f78d9
Brennan T. Beal <44609990+btbeal@users.noreply.github.com>
2024-05-26 18:27:13 -0400

    Moving remaining rules logic out of player and into quoridor (#14)
    Co-authored-by: Brennan Beal <brennan.beal@flatiron.com>

--- src/player.py
+++ src/player.py
@@ -27,132 +27,14 @@ class Player(pygame.sprite.Sprite):
         self.is_ai = is_ai
         self.epsilon = 0.0
 
-    def update(
-        self,
-        event,
-        board: Board,
-        players,
-    ) -> bool:
-        """
-        Returns whether the given event is a valid move and updates the state of the board
-        if so.
-        """
-        if event.type == pygame.KEYDOWN:
-            pressed_keys = pygame.key.get_pressed()
-            current_node = self._current_node(board.nodes)
-            key_list = [key for key in iter(Direction) if pressed_keys[key]]
-            total_keys_pressed = sum(pressed_keys)
-
-            if pressed_keys[pygame.K_a] and key_list and total_keys_pressed <= 2:
-                adjacent_movement = key_list[0]
-                return self._move_adjacent(board, current_node, adjacent_movement)
-
-            if key_list:
-                movement = key_list[0]
-                return self._move(board.nodes, current_node, board.walls, movement)
-
-        #if event.type == pygame.MOUSEBUTTONDOWN:
-        #    pos = pygame.mouse.get_pos()
-        #    walls_to_place = [
-        #        wall for wall in board.walls if wall.rect.collidepoint(pos)
-        #    ]
-        #    num_walls_to_place = len(walls_to_place)
-        #    if not num_walls_to_place:
-        #        return False
-#
-        #    success = Player._place_wall(walls_to_place[0], board, players)
-        #    if success:
-        #        self.total_walls -= 1
-        #        return True
-
-        return False
-
-    def _move(self, nodes, current_node, walls, movement):
-        node_direction = Node.get_coordinates_in_direction(movement)
-        proximal_node = get_proximal_object(self.rect.center, node_direction, nodes)
-
-        wall_direction = Wall.get_coordinates_in_direction(movement)
-        proximal_wall = get_proximal_object(self.rect.center, wall_direction, walls)
-
-        if not proximal_wall or proximal_wall.is_occupied or not proximal_node:
-            return False
-
-        if not proximal_node.is_occupied:
-            self.rect.center = proximal_node.rect.center
-            proximal_node.is_occupied = True
-            current_node.is_occupied = False
-            return True
-
-        next_proximal_wall = get_proximal_object(
-            proximal_node.rect.center, wall_direction, walls
-        )
-        if next_proximal_wall and not next_proximal_wall.is_occupied:
-            next_proximal_node = get_proximal_object(
-                proximal_node.rect.center, node_direction, nodes
-            )
-            self.rect.center = next_proximal_node.rect.center
-            next_proximal_node.is_occupied = True
-            current_node.is_occupied = False
-            return True
-
-        return False
-
-    def _move_adjacent(self, board: Board, current_node, adjacent_movement):
-        current_node_position = current_node.rect.center
-        surrounding_node_dict = board.get_objects_around_node(
-            current_node_position, group=board.nodes, exclude_direction=None
-        )
-
-        occupied_nodes = [
-            (direction, node)
-            for direction, node in surrounding_node_dict.items()
-            if node and node.is_occupied
-        ]
-        if occupied_nodes:
-            occupied_node_information = occupied_nodes[0]
-            occupied_node_object = occupied_node_information[
-                1
-            ]  # in two player game, only ever expect one occupied node
-            occupied_node_direction = occupied_node_information[0]
-            wall_direction = Wall.get_coordinates_in_direction(adjacent_movement)
-            node_direction = Node.get_coordinates_in_direction(adjacent_movement)
-
-            requested_node = get_proximal_object(
-                occupied_node_object.rect.center, node_direction, board.nodes
-            )
-            wall_after_requested_node_direction = tuple(
-                t / 2 for t in occupied_node_direction
-            )
-
-            wall_after_requested_node = get_proximal_object(
-                occupied_node_object.rect.center,
-                wall_after_requested_node_direction,
-                board.walls,
-            )
-            potential_wall_blocking_path = get_proximal_object(
-                occupied_node_object.rect.center, wall_direction, board.walls
-            )
-            if (
-                requested_node
-                and wall_after_requested_node.is_occupied
-                and potential_wall_blocking_path
-                and not potential_wall_blocking_path.is_occupied
-            ):
-                self.rect.center = requested_node.rect.center
-                requested_node.is_occupied = True
-                current_node.is_occupied = False
-                return True
-
-        return False
-
-    def _current_node(self, nodes):
+    def current_node(self, nodes):
         for node in nodes:
             if node.position == self.rect.center:
                 return node
 
         return None
 
-    def place_legal_wall(self, board, validated_wall):
+    def place_wall(self, board, validated_wall):
         adjacent_wall = board.get_adjacent_wall(validated_wall)
         proposed_new_wall = pygame.Rect.union(validated_wall.rect, adjacent_wall.rect)
         validated_wall.is_occupied = True
@@ -161,29 +43,9 @@ class Player(pygame.sprite.Sprite):
         adjacent_wall.kill()
         self.total_walls -= 1
 
-
-    @staticmethod
-    def choose_from_legal_moves(legal_move_dict):
-        random_move_type = np.random.choice(list(legal_move_dict.keys()))
-        potential_coordinates_for_move = legal_move_dict[random_move_type]
-        length_of_set = len(potential_coordinates_for_move)
-        np.random.choice(range(length_of_set))
-
-    def execute_legal_move(self, board, coordinate, move_type):
-        current_node = self._current_node(board.nodes)
-        if move_type == 'move_pawn':
-            new_node = [node for node in board.nodes if board.normalize_coordinates(node.rect.center) == coordinate]
-            new_node = new_node[0]
-            self.rect.center = board.denormalize_coordinates(coordinate)
-            new_node.is_occupied = True
-            current_node.is_occupied = False
-            return True
-        else:
-            for wall in board.walls:
-                if wall.rect.center == coordinate:
-                    self.place_legal_wall(board=board, validated_wall=wall)
-                    self.total_walls -= 1
-                    return True
-
-            return False
+    def move_player(self, new_node, all_nodes):
+        current_node = self.current_node(all_nodes)
+        current_node.is_occupied = False
+        new_node.is_occupied = True
+        self.rect.center = new_node.rect.center
 
--- src/quoridor.py
+++ src/quoridor.py
@@ -17,6 +17,7 @@ from src.constants import (
 from src.node import Node
 from src.player import Player
 from src.render_mixin import RenderMixin
+from src.wall import Wall
 
 
 DEFAULT_FONT_SIZE = 32
@@ -65,19 +66,25 @@ class Quoridor(RenderMixin):
                 while not success:
                     # get_legal_moves
                     # -- get_wall
-                    state = self.board.get_state()
-                    legal_move_dict = self._get_legal_moves(state=state, current_player_index=current_player_index)
-                    random_move_type = np.random.choice(list(legal_move_dict.keys()))
+                    board = self.board
+                    state = board.get_state()
+                    legal_move_dict = self._get_legal_moves(current_player)
+                    eligible_move_types = [key for key, value in legal_move_dict.items() if legal_move_dict[key]]
+                    random_move_type = np.random.choice(eligible_move_types)
                     potential_coordinates_for_move = legal_move_dict[random_move_type]
                     length_of_set = len(potential_coordinates_for_move)
                     index = np.random.choice(range(length_of_set))
                     coords = list(potential_coordinates_for_move)[index]
-                    success = current_player.execute_legal_move(
-                        board=self.board,
-                        coordinate=coords,
-                        move_type=random_move_type
-                    )
+                    if random_move_type == 'place_wall':
+                        wall = next(wall for wall in board.walls if wall.rect.center == coords)
+                        current_player.place_wall(board, wall)
+                    else:
+                        node = next(node for node in board.nodes if node.rect.center == coords)
+                        current_player.move_player(node, board.nodes)
 
+                    success=True
+
+                current_player_index = (current_player_index + 1) % len(self.players)
                 self._render(current_player)
 
             else:
@@ -98,10 +105,29 @@ class Quoridor(RenderMixin):
                                 success = False
 
                             if self._wall_is_legal(wall_to_place) and current_player.total_walls > 0:
-                                current_player.place_legal_wall(self.board, wall_to_place)
+                                current_player.place_wall(self.board, wall_to_place)
                                 success = True
-                        else:
-                            success = current_player.update(event, self.board, self.players)
+                        elif event.type == pygame.KEYDOWN:
+                            pressed_keys = pygame.key.get_pressed()
+                            key_list = [key for key in iter(Direction) if pressed_keys[key]]
+                            total_keys_pressed = sum(pressed_keys)
+
+                            if pressed_keys[pygame.K_a] and key_list and total_keys_pressed <= 2:
+                                adjacent_movement = key_list[0]
+                                legal_adjacent_moves = self._get_legal_adjacent_moves(current_player)
+                                if adjacent_movement in legal_adjacent_moves:
+                                    new_node = legal_adjacent_moves[adjacent_movement]
+                                    current_player.move_player(new_node, self.board.nodes)
+                                    success = True
+
+                            elif key_list:
+                                movement = key_list[0]
+                                legal_lateral_moves = self._get_legal_lateral_moves(current_player)
+                                if movement in legal_lateral_moves:
+                                    new_node = legal_lateral_moves[movement]
+                                    current_player.move_player(new_node, self.board.nodes)
+                                    success = True
+
                     self._render(current_player)
                 current_player_index = (current_player_index + 1) % len(self.players)
 
@@ -109,8 +135,7 @@ class Quoridor(RenderMixin):
     def _is_winner(current_player):
         return TERMINAL_NODE_Y[current_player.index] == current_player.rect.center[1]
 
-    def _get_legal_moves(self, state, current_player_index):
-        current_player = self.players[current_player_index]
+    def _get_legal_moves(self, current_player):
         eligible_wall_coords = []
         if current_player.total_walls > 0:
             walls = self._get_legal_walls()
@@ -118,11 +143,15 @@ class Quoridor(RenderMixin):
             for wall in walls:
                 eligible_wall_coords.append(wall.rect.center)
 
-        eligible_node_coords = self._get_eligible_node_coords(state, current_player_index)
+        eligible_lateral_moves = self._get_legal_lateral_moves(current_player)
+        eligible_lateral_move_coords = [node.rect.center for direction, node in eligible_lateral_moves.items() if node]
+        eligible_adjacent_moves = self._get_legal_adjacent_moves(current_player)
+        eligible_adjacent_move_coords = [node.rect.center for direction, node in eligible_adjacent_moves.items() if node]
+        all_eligible_move_coords = eligible_lateral_move_coords + eligible_adjacent_move_coords
 
         move_dict = {
             'place_wall': eligible_wall_coords,
-            'move_pawn': eligible_node_coords
+            'move_pawn': all_eligible_move_coords
         }
 
         return move_dict
@@ -207,3 +236,52 @@ class Quoridor(RenderMixin):
                                     eligible_coords.add(nodes_around_opponent_node_dict[direct])
 
         return eligible_coords
+
+    def _get_legal_adjacent_moves(self, current_player):
+        other_player_direction = self.board.get_direction_of_proximal_player(current_player)
+        board = self.board
+        eligible_adjacent_directions = {}
+        if other_player_direction:
+            other_player_node_dict = board.get_nodes_around_node(current_player.rect.center, [other_player_direction])
+            other_player_node = other_player_node_dict[other_player_direction]
+            adjacent_directions = Direction.get_adjacent_directions(other_player_direction)
+            walls_around_player_node_dict = board.get_walls_around_node(current_player.rect.center, [other_player_direction])
+            wall_between_players = walls_around_player_node_dict[other_player_direction]
+            if wall_between_players and not wall_between_players.is_occupied:
+                for direction in adjacent_directions:
+                    is_legal_move, node = self._is_legal_lateral_move(direction, other_player_node.rect.center)
+                    if is_legal_move:
+                        eligible_adjacent_directions[direction] = node
+
+        return eligible_adjacent_directions
+
+    def _get_legal_lateral_moves(self, current_player):
+        legal_lateral_moves = {}
+        starting_node = current_player.rect.center
+        for direction in Direction:
+            is_legal_move, node = self._is_legal_lateral_move(direction, starting_node)
+            if is_legal_move:
+                legal_lateral_moves[direction] = node
+
+        return legal_lateral_moves
+
+    def _is_legal_lateral_move(self, direction, starting_node):
+        board = self.board
+        proximal_node_dict = board.get_nodes_around_node(starting_node, [direction])
+        proximal_node = proximal_node_dict[direction]
+
+        proximal_wall_dict = board.get_walls_around_node(starting_node, [direction])
+        proximal_wall = proximal_wall_dict[direction]
+        if not proximal_wall or proximal_wall.is_occupied or not proximal_node:
+            return False, None
+
+        if not proximal_node.is_occupied:
+            return True, proximal_node
+
+        next_proximal_wall_dict = board.get_walls_around_node(proximal_node.rect.center, [direction])
+        if next_proximal_wall_dict[direction] and not next_proximal_wall_dict[direction].is_occupied:
+            next_proximal_node_dict = board.get_nodes_around_node(proximal_node.rect.center, [direction])
+            next_proximal_node = next_proximal_node_dict[direction]
+            return True, next_proximal_node
+
+        return False, None
