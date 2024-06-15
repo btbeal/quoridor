d32cc7682c12ba04cc403e87065430b6e0b45a01
Brennan T. Beal <44609990+btbeal@users.noreply.github.com>
2024-06-15 14:03:25 -0400

    Renaming (#24)

--- gym.py
+++ gym.py
@@ -3,4 +3,4 @@ from src.quoridor import QuoridorGym
 
 if __name__ == "__main__":
     gym = QuoridorGym(model_filenames={0: 'agent_0.pth', 1: 'agent_1.pth'})
-    gym.run_ai_gym()
+    gym.run_training_session()
